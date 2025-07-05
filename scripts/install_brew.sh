b302e4b0b801ce58d0219831711e46aae3b3ad35
Minimal Devops <109197600+MinimalDevops@users.noreply.github.com>
2025-07-05 12:12:42 -0700

    Enhance setup scripts with missing mac steps (#2)

--- scripts/install_brew.sh
+++ scripts/install_brew.sh
@@ -10,7 +10,13 @@ fi
 OS="$(uname -s)"
 
 if [[ "$OS" == "Darwin" ]]; then
+    if ! xcode-select -p >/dev/null 2>&1; then
+        echo "Installing Xcode command line tools..."
+        xcode-select --install
+    fi
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 elif [[ "$OS" == "Linux" ]]; then
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
